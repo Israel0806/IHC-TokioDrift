81c1734370be4310268501c8e201096f104dad70
Israel0806 <israel0806_@hotmail.com>
2020-11-07 19:16:47 -0500

    jugador listo, parte 1

---
+++ TokioDrift/Assets/Scripts/Mobs/MoveChicken.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using TMPro;
+using System.Diagnostics;
+
+public class MoveChicken : MonoBehaviour
+{
+    public float[] Angles;
+    public GameObject Chicken;
+    public float RayDistance = 1f;
+    public Animator ChickenAnimator;
+    public LayerMask TrackLayer;
+    public LayerMask PlayerLayer;
+    public float RunSpeed = 2;
+    private bool CanRun = true;
+    private bool Hitted = false;
+    public TextMeshProUGUI score;
+    public AudioClip DeathSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!CanRun) return;
+
+        // Move the object forward along its z axis 1 unit/second.
+        if(ChickenAnimator.GetCurrentAnimatorStateInfo(0).IsName("Run In Place"))
+            transform.Translate(Vector3.forward * Time.deltaTime * RunSpeed);
+
+        Vector3 origin = transform.position;
+        //Vector3 originPlayer = new Vector3(transform.position.x, 0.3f, transform.position.z);
+
+        for (int i = 0; i < Angles.Length; i++)
+        {
+            Vector3 direction = GetDirectionFromAngle(Angles[i], Vector3.up, transform.forward);
+
+            // hit the wall
+            RaycastHit hit;
+            UnityEngine.Debug.DrawRay(origin, direction * 1, Color.yellow);
+            if (Physics.Raycast(origin, direction, out hit, RayDistance, TrackLayer))
+                this.transform.Rotate(0f, 180f , 0f, Space.World);
+            // hitted by the player
+            if (!Hitted && Physics.Raycast(origin, direction, out hit, RayDistance, PlayerLayer)) {
+                if (DeathSound)
+                {
+                    AudioUtility.CreateSFX(DeathSound, transform.position, AudioUtility.AudioGroups.Collision, 0f);
+                }
+
+                float _score = int.Parse(score.text);
+                score.text = (_score + 100).ToString();
+
+                Hitted = true;
+                CanRun = false;
+                this.transform.localScale += new Vector3(3f, 0f, -1f);
+                this.transform.Rotate(0f, -180f, 0f, Space.World);
+                this.transform.Rotate(-90f, 0f, 0f, Space.World);
+                Invoke("KillChicken", 3);
+            }
+        }
+    }
+
+    void KillChicken()
+    {
+        ChickenAnimator.ResetTrigger("Revive");
+        ChickenAnimator.SetTrigger("Die");
+        Invoke("ReviveChicken", 2);
+    }
+
+    void ReviveChicken()
+    {
+        Hitted = false;
+        CanRun = true;
+        this.transform.localScale += new Vector3(-3f, 0f, 1f);
+        this.transform.Rotate(90f, 0, 0f, Space.World);
+        this.transform.Rotate(0f, 180f, 0f, Space.World);
+
+        ChickenAnimator.ResetTrigger("Die");
+        ChickenAnimator.SetTrigger("Revive");
+    }
+
+    Vector3 GetDirectionFromAngle(float degrees, Vector3 axis, Vector3 zerothDirection)
+    {
+        Quaternion rotation = Quaternion.AngleAxis(degrees, axis);
+        return (rotation * zerothDirection);
+    }
+}
---
+++ TokioDrift/Assets/Scripts/Network/Kart/KartController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using Mirror;
+using System.Collections.Generic;
+using Cinemachine;
+using KartGame.KartSystems;
+
+/*
+	Documentation: https://mirror-networking.com/docs/Guides/NetworkBehaviour.html
+	API Reference: https://mirror-networking.com/docs/api/Mirror.NetworkBehaviour.html
+*/
+
+public class KartController : NetworkBehaviour
+{
+    private ArcadeKart arcadeKart;
+    private NetworkManager networkManager;
+    private CinemachineVirtualCamera camera;
+    private GameObject gameManager;
+
+    [Header("Game Stats")]
+    [SyncVar]
+    public int score;
+
+
+
+    #region Start & Stop Callbacks
+
+
+    /// <summary>
+    /// This is invoked on clients when the server has caused this object to be destroyed.
+    /// <para>This can be used as a hook to invoke effects or do client specific cleanup.</para>
+    /// </summary>
+    public override void OnStopClient() { }
+
+    /// <summary>
+    /// Called when the local player object has been set up.
+    /// <para>This happens after OnStartClient(), as it is triggered by an ownership message from the server. This is an appropriate place to activate components or functionality that should only be active for the local player, such as cameras and input.</para>
+    /// </summary>
+    public override void OnStartLocalPlayer() {
+        networkManager = GameObject.Find("NetworkManager").GetComponent<NetworkManagerCar>();
+        camera = GameObject.Find("CinemachineVirtualCamera").GetComponent<CinemachineVirtualCamera>();
+        gameManager = GameObject.Find("GameManager");
+        arcadeKart = this.GetComponent<ArcadeKart>();
+
+        camera.m_Follow = this.transform;
+        camera.m_LookAt = this.transform;
+        //gameManager.GetComponent<GameFlowManager>().playerKart = this.GetComponent<ArcadeKart>();
+        
+        arcadeKart.SetCanMove(false);
+
+        if (networkManager.numPlayers == 1)
+            gameManager.GetComponent<GameFlowManager>().GameReady();
+
+    }
+    #endregion
+
+
+    [Client]
+    void Update()
+    {
+        if (!isLocalPlayer) return;
+        
+    }
+
+
+}
